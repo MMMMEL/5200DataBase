e6fa01aabc9c39e7640916670c32d37ef03f7d7d
esandar <31935897+esandar@users.noreply.github.com>
2018-07-11 16:19:14 -0700

    Add files via upload

--- EventSmart/src/event/dal/ConnectionManager.java
+++ EventSmart/src/event/dal/ConnectionManager.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-import javax.servlet.ServletException;
+//import javax.servlet.ServletException;
 
 /**
  * Use ConnectionManager to connect to your database instance.
@@ -38,7 +38,7 @@ public class ConnectionManager {
 	// User to connect to your database instance. By default, this is "root2".
 	private final String user = "root";
 	// Password for the user.
-	private final String password = "password";
+	private final String password = "yuge3398";
 	// URI to your database server. If running on the same machine, then this is "localhost".
 	private final String hostName = "localhost";
 	// Port to your database server. By default, this is 3307.
@@ -53,17 +53,14 @@ public class ConnectionManager {
 			Properties connectionProperties = new Properties();
 			connectionProperties.put("user", this.user);
 			connectionProperties.put("password", this.password);
-			
-			//self-added
-			connectionProperties.put("useSSL", "false");
-			connectionProperties.put("useUnicode", "true");
-			connectionProperties.put("useJDBCCompliantTimezoneShift", "true");
-			connectionProperties.put("useLegacyDatetimeCode","false");
-			connectionProperties.put("serverTimezone", "PST");
-			
 			// Ensure the JDBC driver is loaded by retrieving the runtime Class descriptor.
 			// Otherwise, Tomcat may have issues loading libraries in the proper order.
 			// One alternative is calling this in the HttpServlet init() override.
+			connectionProperties.put("useSSL", "false");
+			connectionProperties.put("useUnicode", "true");
+			connectionProperties.put("useJDBCCompliantTimezoneShift", "true");
+			connectionProperties.put("useLegacyDatetimeCode", "false");
+			connectionProperties.put("serverTimezone", "PST");
 			try {
 				Class.forName("com.mysql.cj.jdbc.Driver");
 			} catch (ClassNotFoundException e) {
@@ -90,3 +87,4 @@ public class ConnectionManager {
 		}
 	}
 }
+
